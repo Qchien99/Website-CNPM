07461af3a1f2d0e4b45ed942afaaf39a0d021dc8
daovanhai <1851161809@e.tlu.edu.vn>
2021-05-16 01:12:41 +0700

    tạo và thêm dữ liệu cho bảng products

--- weblaptop (1).sql
+++ weblaptop (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 14, 2021 lúc 07:50 AM
+-- Thời gian đã tạo: Th5 15, 2021 lúc 08:11 PM
 -- Phiên bản máy phục vụ: 10.3.16-MariaDB
 -- Phiên bản PHP: 7.3.6
 
@@ -21,6 +21,37 @@ SET time_zone = "+00:00";
 --
 -- Cơ sở dữ liệu: `weblaptop`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `parent_id` int(11) NOT NULL DEFAULT 0,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `parent_id`, `slug`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'LAPTOP', 0, 'laptop', '2021-05-13 23:22:30', '2021-05-13 23:22:30', NULL),
+(2, 'ASUS', 1, 'asus', '2021-05-13 23:28:38', '2021-05-13 23:29:57', NULL),
+(3, 'ASUS Gaming', 2, 'asus-gaming', '2021-05-13 23:31:58', '2021-05-13 23:31:58', NULL),
+(4, 'đào văn hải', 3, 'dao-van-hai', '2021-05-13 23:33:19', '2021-05-13 23:33:54', '2021-05-13 23:33:54'),
+(5, 'đào văn hải', 0, 'dao-van-hai', '2021-05-13 23:34:49', '2021-05-13 23:34:55', '2021-05-13 23:34:55'),
+(6, 'ĐIỆN THOẠI', 0, 'dien-thoai', '2021-05-13 23:36:01', '2021-05-13 23:36:01', NULL),
+(7, 'OPPO', 6, 'oppo', '2021-05-13 23:36:15', '2021-05-13 23:36:15', NULL),
+(8, 'IPHONE', 6, 'iphone', '2021-05-13 23:36:30', '2021-05-13 23:36:30', NULL),
+(9, 'IPHONE 6s', 8, 'iphone-6s', '2021-05-13 23:38:06', '2021-05-13 23:38:06', NULL);
 
 -- --------------------------------------------------------
 
@@ -58,7 +89,10 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2021_05_11_134531_add_column_deleted_at_table_users', 2);
+(4, '2021_05_11_134531_add_column_deleted_at_table_users', 2),
+(5, '2021_05_14_055309_create_categories_table', 3),
+(6, '2021_05_14_062445_add_column_deleted_at_table_categories', 4),
+(7, '2021_05_15_153040_create_products_table', 5);
 
 -- --------------------------------------------------------
 
@@ -71,6 +105,33 @@ CREATE TABLE `password_resets` (
   `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `code` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` decimal(18,2) NOT NULL,
+  `state` tinyint(4) NOT NULL,
+  `categories_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `code`, `slug`, `image`, `price`, `state`, `categories_id`, `created_at`, `updated_at`) VALUES
+(9, 'đào văn hải', 'YOB4HQ0', 'dao-van-hai', 'dao-van-hai.PNG', '1500000.00', 1, 2, '2021-05-15 11:05:37', '2021-05-15 11:05:37'),
+(10, 'IPHONE 6s', 'sp01', 'iphone-6s', 'iphone-6s.jpg', '1500000.00', 1, 8, '2021-05-15 11:09:02', '2021-05-15 11:09:02');
 
 -- --------------------------------------------------------
 
@@ -112,6 +173,12 @@ INSERT INTO `users` (`id`, `fullname`, `password`, `address`, `email`, `phone`, 
 --
 
 --
+-- Chỉ mục cho bảng `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Chỉ mục cho bảng `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -131,6 +198,14 @@ ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
+-- Chỉ mục cho bảng `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `products_code_unique` (`code`),
+  ADD KEY `products_categories_id_foreign` (`categories_id`);
+
+--
 -- Chỉ mục cho bảng `users`
 --
 ALTER TABLE `users`
@@ -139,6 +214,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT cho các bảng đã đổ
 --
+
+--
+-- AUTO_INCREMENT cho bảng `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT cho bảng `failed_jobs`
@@ -150,13 +231,29 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT cho bảng `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT cho bảng `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_categories_id_foreign` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
